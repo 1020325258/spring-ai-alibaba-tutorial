0d170552123fbefc0281eec4997911be8ad85485
zhaiqianyi002 <zhaiqianyi002@ke.com>
2025-10-12 22:05:19 +0800

    refactor：rag代码优化

--- 02-spring-ai-alibaba-rag/src/test/java/com/alibaba/yycome/ragTest.java
+++ 02-spring-ai-alibaba-rag/src/test/java/com/alibaba/yycome/ragTest.java
@@ -3,43 +3,35 @@ package com.alibaba.yycome;
 import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
 import com.alibaba.cloud.ai.dashscope.rag.DashScopeDocumentRetriever;
 import com.alibaba.cloud.ai.dashscope.rag.DashScopeDocumentRetrieverOptions;
-import com.alibaba.cloud.ai.graph.CompiledGraph;
-import com.alibaba.cloud.ai.graph.NodeOutput;
-import com.alibaba.cloud.ai.graph.StateGraph;
 import com.alibaba.cloud.ai.graph.exception.GraphStateException;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.rag.Query;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.codec.ServerSentEvent;
-import reactor.core.Disposable;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Sinks;
 
 import java.util.List;
 
 @SpringBootTest
-public class Test {
+public class ragTest {
 
-    Logger logger = LoggerFactory.getLogger(Test.class);
+    Logger logger = LoggerFactory.getLogger(ragTest.class);
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void test_rag_retrieval() throws GraphStateException {
         DashScopeApi dashScopeApi = DashScopeApi.builder().apiKey(System.getenv("AI_DASHSCOPE_API_KEY")).build();
         DashScopeDocumentRetriever documentRetriever = new DashScopeDocumentRetriever(dashScopeApi, DashScopeDocumentRetrieverOptions.builder()
+                // IndexName 就是数据库的名称
                 .withIndexName("面试解析生成")
                 // 最小相关性分数阈值
                 .withRerankMinScore(0.6f)
+                // 不重写结果
                 .withEnableRewrite(false)
                 .build());
         List<Document> retrieve = documentRetriever.retrieve(new Query("sql的更新流程"));
         for (Document document : retrieve) {
-            logger.info(document.toString());
+            logger.info(document.getText());
         }
     }
 }
