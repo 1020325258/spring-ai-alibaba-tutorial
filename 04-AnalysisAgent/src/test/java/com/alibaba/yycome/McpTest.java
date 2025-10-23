b73bfa7f4d6cd28c8ca3772613082b2c19913a40
zhaiqianyi002 <zhaiqianyi002@ke.com>
2025-10-24 00:12:26 +0800

    feature：增加 aliyun tool calling 能力

--- 04-AnalysisAgent/src/test/java/com/alibaba/yycome/McpTest.java
+++ 04-AnalysisAgent/src/test/java/com/alibaba/yycome/McpTest.java
@@ -1,8 +1,12 @@
 package com.alibaba.yycome;
 
+import com.alibaba.cloud.ai.toolcalling.aliyunaisearch.AliyunAiSearchService;
+import com.alibaba.cloud.ai.toolcalling.baidusearch.BaiduAiSearchService;
+import com.alibaba.cloud.ai.toolcalling.common.interfaces.SearchService;
 import com.alibaba.yycome.service.McpService;
 import io.modelcontextprotocol.spec.McpSchema;
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,15 +18,30 @@ public class McpTest {
     @Autowired
     McpService mcpService;
 
+    @Autowired
+    ChatClient searchAgent;
+
     @Test
     public void test() {
-        List<McpSchema.Content> query = mcpService.query("mysql 的面试题有哪些?");
+        String query = "明确 MySQL 支持的事务隔离级别（READ UNCOMMITTED、READ COMMITTED、REPEATABLE READ、SERIALIZABLE），收集每种隔离级别的定义、行为特征、解决的并发问题（脏读、不可重复读、幻读）以及 MySQL 默认隔离级别。同时调研 InnoDB 引擎对各隔离级别的具体实现机制（如 MVCC、Next-Key Lock 等）。";
 
-        McpSchema.Content content = query.getLast();
-        if ("text".equals(content.type())) {
-            content = (McpSchema.TextContent) content;
-            System.out.println(content);
-        }
+        System.out.println(searchAgent.prompt(query).call().content());
+
+        String query2 = "\"收集面试中关于事务隔离级别的常见错误回答（如混淆幻读与不可重复读、误认为 REPEATABLE READ 能完全避免幻读）、典型变体问题（如‘RR 级别下是否会出现幻读？’‘如何手动避免幻读？’），并分析面试官通过此题考察的维度（数据库基础、并发控制理解、实战调优意识）。";
+        System.out.println(searchAgent.prompt(query2).call().content());
+
     }
 
+    @Autowired
+    private AliyunAiSearchService searchService;
+
+    /**
+     * 需从阿里云申请 api-key，网址：https://opensearch.console.aliyun.com/cn-shanghai/rag/experience-center?serverType=web-search&serverId=ops-web-search-001
+     * 阿里云提供的 search 能力较弱
+     */
+    @Test
+    public void testAliyunSearch() {
+        SearchService.SearchResult searchResult = searchService.query("mysql 的面试题有哪些？").getSearchResult();
+        searchResult.results().stream().forEach(System.out::println);
+    }
 }
